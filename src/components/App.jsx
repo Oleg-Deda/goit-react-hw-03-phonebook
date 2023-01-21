3085acf61e6a38e89dcdd0d99036e3488d265824
Oleg-Deda <dedaoleg1983@gmail.com>
2023-01-21 18:12:40 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,19 +4,20 @@ import { Title } from './Title/Title';
 import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactsList } from './ContactsList/ContactsList';
-
 import { nanoid } from 'nanoid';
 
-
 const INITIAL_STATE = {
-  contacts: [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ],
+  contacts: [],
   filter: '',
+      
 };
+
+const INITIAL_CONTACTS = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 
 const Titles = {
   form: 'Phonebook',
@@ -25,6 +26,22 @@ const Titles = {
 
 export class App extends Component {
   state = { ...INITIAL_STATE };
+
+componentDidMount() {
+  const parsedContacts = JSON.parse(localStorage.getItem('contacts'));
+  if (parsedContacts){
+    this.setState({contacts: parsedContacts});
+      }
+  else {
+    this.setState({contacts: INITIAL_CONTACTS});
+  }
+}
+
+componentDidUpdate(_, prevState) {
+  if (this.state.contacts !== prevState.contacts) {
+    localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  }
+}
 
   addContact = (name, number) => {
     if (this.state.contacts.find(contact => contact.name === name)) {
